7604f7727c528ae737d10abd3580b5bacd7826dc
Nargiz H. Mammadzada <nargizhma@code.local>
2025-11-15 19:51:07 +0400

    2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,21 @@
-﻿Console.WriteLine("Hello, World");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine(a);
+﻿// ad
+Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+
+//soyad
+Console.WriteLine("Enter your Last name");
+string surname = Console.ReadLine();
+
+// ad gun tarix
+
+Console.WriteLine("Enter your date of birth");
+DateOnly time = DateOnly.Parse(Console.ReadLine());
+int age = DateOnly.FromDateTime(DateTime.Now).Year - time.Year;
+
+// gender
+
+Console.WriteLine("Enter your gender");
+string gender = Console.ReadLine();
+
+// printing shi
+Console.WriteLine($"name: {name} \nsurname: {surname} \nage: {age} \ngender: {gender}");
